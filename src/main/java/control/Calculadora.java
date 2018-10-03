807eb71695c94b0cbb0fe106e4da825cdbb3adc0
admin7k <admin7k@admin7k-pc>
2018-10-03 12:41:54 +0200

    inicio 03.10.2018

---
+++ src/main/java/control/Calculadora.java
@@ -0,0 +1,160 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+
+import net.daw.bean.Respuesta;
+import net.daw.bean.Resultado;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.google.gson.Gson;
+import static java.lang.System.out;
+import net.daw.bean.Errores;
+
+/**
+ *
+ * @author Alejandro
+ */
+public class Calculadora extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("application/jSon");
+        PrintWriter out = response.getWriter();
+
+        String operacion;
+        Double oper1 = null;
+        Double oper2 = null;
+        Double res = null;
+        Gson gSon = new Gson();
+        Boolean fallo = false;
+
+        operacion = request.getParameter("operador");
+
+        try {
+            oper1 = Double.parseDouble(request.getParameter("op1"));
+            oper2 = Double.parseDouble(request.getParameter("op2"));
+
+        } catch (NumberFormatException nfex) {
+            response.setStatus(500);
+            Errores error = new Errores();
+            error.setOper1(oper1);
+            error.setOper2(oper2);
+            error.setOperacion(operacion);
+            error.setMensaje("Por favor, ingrese dos numeros en el formulario");
+
+            response.getWriter().append(gSon.toJson(error));
+
+        }
+
+        
+            try {
+
+                switch (operacion) {
+                    case "sum":
+                        res = oper1 + oper2;
+                        break;
+                    case "rest":
+                        res = oper1 - oper2;
+                        break;
+                    case "mult":
+                        res = oper1 * oper2;
+                        break;
+                    case "div":
+                        if (oper2 == 0) {
+                            response.setStatus(500);
+                            Errores error = new Errores();
+                            error.setOper1(oper1);
+                            error.setOper2(oper2);
+                            error.setOperacion(operacion);
+                            error.setMensaje("No se puede dividir por cero");
+                            response.getWriter().append(gSon.toJson(error));
+                            fallo = true;
+                        } else {
+                            res = oper1 / oper2;
+                        }
+
+                        break;
+                    default:
+                        response.setStatus(500);
+                        Errores error = new Errores();
+                        error.setOper1(oper1);
+                        error.setOper2(oper2);
+                        error.setOperacion(operacion);
+                        error.setMensaje("No ha especificado una operación");
+                        response.getWriter().append(gSon.toJson(error));
+                        fallo = true;
+                }
+                if (!fallo) {
+                    Resultado r = new Resultado();
+                    r.setOp1(oper1);
+                    r.setOp2(oper2);
+                    r.setResultado(res);
+
+                    response.getWriter().append(gSon.toJson(r));
+                }
+
+            } finally {
+                //response.getWriter().append(gSon.toJson("this is the end, my only friend, the end"));
+                out.close();
+            }
+        
+    }
+
+// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ src/main/java/net/daw/bean/Respuesta.java
@@ -0,0 +1,36 @@
+package net.daw.bean;
+
+
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author a007243984l
+ */
+public class Respuesta {
+    Integer i;
+    ArrayList array;
+
+    public Integer getI() {
+        return i;
+    }
+
+    public void setI(Integer i) {
+        this.i = i;
+    }
+
+    public ArrayList getArray() {
+        return array;
+    }
+
+    public void setArray(ArrayList array) {
+        this.array = array;
+    }
+    
+}
